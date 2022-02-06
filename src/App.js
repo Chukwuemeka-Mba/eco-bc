04d0d3970a2f19a5595a3e37452b8cb3b53bd34f
Chukwuemeka Mba <76162087+Chukwuemeka-Mba@users.noreply.github.com>
2022-02-06 11:49:58 -0500

    testing router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import React from "react";
 import Navbar from "./components/Navbar/Navbar";
 import Footer from "./components/Footer/Footer";
+import Login from "./components/login/login";
+import Signup from "./components/signup/signup";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-      <Footer />
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <Footer />
+        <div className="">
+          <Switch>
+            <Route path="/login">
+              <Login />
+            </Route>
+          </Switch>
+        </div>
+      </div>
+    </Router>
   );
 }
 
